dc3dc5145212ece9162e6a1097a02aa9060c9e6a
akshatakanaje <111020296+akshatakanaje@users.noreply.github.com>
2025-03-21 16:29:26 -0700

    added GET, POST, PUT, DELETE request class

---
+++ src/test/java/httpRequest/PostRequest.java
@@ -0,0 +1,33 @@
+package httpRequest;
+
+import io.restassured.RestAssured;
+import io.restassured.config.LogConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+
+public class PostRequest extends BaseRequest{
+
+    @Override
+    public RequestSpecification buildRequest() {
+
+
+        if (baseUrl == null) {
+            throw new IllegalStateException("Required fields are not set.");
+        }
+
+        RestAssured.config().logConfig(new LogConfig().defaultStream(getLogPrintStream()));
+
+        return RestAssured.given()
+                .baseUri(baseUrl)
+                .filters(new RequestLoggingFilter(LogDetail.ALL, getLogPrintStream()), // Ensure logging to the same PrintStream
+                        new ResponseLoggingFilter(LogDetail.BODY, getLogPrintStream())) // Log response body
+                .header("Content-Type",acceptHeader)
+                .header("Accept",acceptHeader)
+                .header("Authorization","Bearer "+token)
+                .body(requestBody);
+
+
+    }
+}
